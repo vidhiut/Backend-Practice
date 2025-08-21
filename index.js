16bc3c8d158dfcbb6659d3e7f794667848890d44
vidhiut <vidhitalegaonkar@gmail.com>
2025-08-21 19:22:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/login', (req,res)=>{
+    res.send("login dun is working")
+})
+
+app.get('/home', (req,res)=>{
+    res.send("<h1> welcome to my website</h1>")
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
